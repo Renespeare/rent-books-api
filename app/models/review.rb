52c4709ba687fb7e04940748952363649d69278d
nefarha <61551675+nefarha@users.noreply.github.com>
2022-07-01 23:01:23 +0700

    Penambahan agar star kurang dari atau sama dengan 5 dan membuat 1 user hanya bisa sekali review saja

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,5 @@
 class Review < ApplicationRecord
     belongs_to :book
     belongs_to :user
-    validates :star, length: {maximum: 5}
+    validates :star, numericality: { less_than_or_equal_to: 5,  only_integer: true }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_29_192609) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_01_154644) do
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "synopsis"
@@ -40,6 +40,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_29_192609) do
     t.text "comment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["book_id", "user_id"], name: "index_reviews_on_book_id_and_user_id", unique: true
   end
 
   create_table "users", force: :cascade do |t|
